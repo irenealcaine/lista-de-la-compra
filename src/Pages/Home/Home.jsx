98c23220d0a66bd41ac9079035940fd387fd3265
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-03 15:38:57 +0200

    minor fixes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,51 +1,28 @@
-import BlogCard from "../../Components/BlogCard/BlogCard";
-import Button from "../../Components/Button/Button";
 import Checkbox from "../../Components/Checkbox/Checkbox";
-import Loader from "../../Components/Loader/Loader";
-import ProductCard from "../../Components/ProductCard/ProductCard";
-import { posts } from "../../Data/BlogPosts";
 import { products } from "../../Data/Products";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
-      <h1>Lista de la compra</h1>
-      <h2>¿Qúe hace falta?</h2>
+      <h1>¿Qué hace falta?</h1>
+
       {products.map((category) => (
         <div>
-          <h3>{category.title}</h3>
+          <h2>{category.title}</h2>
+
           <ul>
-
-
             {category.items.map((item) => (
               <li>
                 <Checkbox isChecked={item.toBuy} />
                 {item.name}
               </li>
             ))}
-
           </ul>
-        </div>
 
+        </div>
       ))}
 
-      <div className="buttons-container">
-        <Button value={"Default"} href={"https://google.es"} />
-        <Button
-          value={"Secondary"}
-          href={"https://google.es"}
-          color={"secondary"}
-        />
-        <Button
-          value={"Disabled"}
-          href={"https://google.es"}
-          color={"purple"}
-          disabled={true}
-        />
-      </div>
-
-      <Loader />
     </div>
   );
 };
--- src/Pages/Page1/Page1.jsx
+++ src/Pages/Page1/Page1.jsx
@@ -1,4 +1,3 @@
-import Checkbox from "../../Components/Checkbox/Checkbox";
 import { products } from "../../Data/Products";
 import "./Page1.scss";
 
@@ -7,10 +6,9 @@ const Page1 = () => {
   return (
     <div className="page1">
       <h1>Lista de la compra</h1>
-      <h2>¿Qué hay que comprar?</h2>
       {products.map((category) => (
         <div>
-          <h3>{category.title}</h3>
+          <h2>{category.title}</h2>
           <ul>
             {category.items.filter(item => item.toBuy).map((item) => (
               <li>
