fb49196a586d694890bd87856dcd51df9c177814
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-03 15:34:55 +0200

    add to buy page

--- src/Pages/Page1/Page1.jsx
+++ src/Pages/Page1/Page1.jsx
@@ -1,9 +1,27 @@
+import Checkbox from "../../Components/Checkbox/Checkbox";
+import { products } from "../../Data/Products";
 import "./Page1.scss";
 
 const Page1 = () => {
+
   return (
     <div className="page1">
-      <h1>Page 1</h1>
+      <h1>Lista de la compra</h1>
+      <h2>¿Qué hay que comprar?</h2>
+      {products.map((category) => (
+        <div>
+          <h3>{category.title}</h3>
+          <ul>
+            {category.items.filter(item => item.toBuy).map((item) => (
+              <li>
+                {item.name}
+              </li>
+            ))}
+
+          </ul>
+        </div>
+
+      ))}
     </div>
   );
 };
