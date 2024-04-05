506be7ce4f6317da11703db62a27c598651a376c
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-05 16:12:06 +0200

    update TextInput.jsx, IniciarSesion.jsx and IniciarSesion.scss

--- src/Components/TextInput/TextInput.jsx
+++ src/Components/TextInput/TextInput.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './TextInput.scss'
 
-const TextInput = ({ type, placeholder }) => {
+const TextInput = ({ type, placeholder, onChange }) => {
   return (
-    <input className='text-input' type={type} placeholder={placeholder} />
+    <input className='text-input' type={type} placeholder={placeholder} onChange={onChange} />
   )
 }
 
--- src/Pages/IniciarSesion/IniciarSesion.jsx
+++ src/Pages/IniciarSesion/IniciarSesion.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
 import TextInput from '../../Components/TextInput/TextInput'
 import './IniciarSesion.scss'
+import Button from '../../Components/Button/Button'
+
+import React, { useState } from 'react'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate, Link } from 'react-router-dom';
 
 const IniciarSesion = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate()
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const auth = getAuth();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate('/')
+    } catch (error) {
+      setError("Error");
+    }
+  };
+
   return (
     <div className='iniciar-sesion'>
       <h1>Iniciar sesión</h1>
-      <form className='form'>
-        <TextInput placeholder={'Correo electrónico'} />
-        <TextInput type={'password'} placeholder={'Contraseña'} />
+      <form className='form' onSubmit={handleLogin}>
+        <TextInput placeholder={'Correo electrónico'} onChange={(e) => setEmail(e.target.value)} />
+        <TextInput type={'password'} placeholder={'Contraseña'} onChange={(e) => setPassword(e.target.value)} />
+        <Button type={'submit'} value={'Iniciar sesión'} />
+        {error && <p className='error'>{error}</p>}
         <p>¿No tienes cuenta? <a href='#'>Crear una</a></p>
       </form>
     </div>
