680b884f0e10e3adb0565b75352d0ab469fbbd6b
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-06 16:13:32 +0200

    update CrearCuenta.jsx

--- src/Pages/CrearCuenta/CrearCuenta.jsx
+++ src/Pages/CrearCuenta/CrearCuenta.jsx
@@ -4,7 +4,7 @@ import TextInput from '../../Components/TextInput/TextInput'
 import Button from '../../Components/Button/Button'
 
 import React, { useState } from 'react'
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { Link, useNavigate } from 'react-router-dom';
 
 const CrearCuenta = () => {
@@ -14,21 +14,22 @@ const CrearCuenta = () => {
   const [error, setError] = useState('');
   const navigate = useNavigate()
 
-  const handleLogin = async (e) => {
+  const handleCreate = async (e) => {
     e.preventDefault();
     const auth = getAuth();
     try {
-      await signInWithEmailAndPassword(auth, email, password);
-      navigate('/')
+      await createUserWithEmailAndPassword(auth, email, password);
+      navigate('/iniciar-sesion')
     } catch (error) {
       setError("Error");
+      console.log(error)
     }
   };
 
   return (
     <div className='crear-cuenta'>
       <h1>Crear cuenta</h1>
-      <form className='form' onSubmit={handleLogin}>
+      <form className='form' onSubmit={handleCreate}>
         <TextInput placeholder={'Correo electrónico'} onChange={(e) => setEmail(e.target.value)} />
         <TextInput type={'password'} placeholder={'Contraseña'} onChange={(e) => setPassword(e.target.value)} />
         <Button type={'submit'} value={'Crea cuenta'} />
